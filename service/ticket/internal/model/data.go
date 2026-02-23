4c011915a7d59138e5fa6efa053c44e7d8903483
dwiki <kusumabriantono@gmail.com>
2026-02-23 23:57:51 +0700

    add json tag

--- service/ticket/internal/model/data.go
+++ service/ticket/internal/model/data.go
@@ -6,49 +6,49 @@ import (
 )
 
 type EventData struct {
-	ID          int32
-	Name        string
-	Description string
-	Location    string
-	StartTime   time.Time
-	EndTime     time.Time
+	ID          int32     `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description,omitempty"`
+	Location    string    `json:"location"`
+	StartTime   time.Time `json:"start_time"`
+	EndTime     time.Time `json:"end_time"`
 }
 
 type FileData struct {
-	Filename     string
-	Size         int64
-	ContentType  string
-	Reader       io.Reader
-	IsPrimary    bool
-	DisplayOrder int
+	Filename     string    `json:"filename"`
+	Size         int64     `json:"size"`
+	ContentType  string    `json:"contentType"`
+	Reader       io.Reader `json:"reader"`
+	IsPrimary    bool      `json:"is_primary"`
+	DisplayOrder int       `json:"display_order"`
 }
 
 type InsertTicketRequest struct {
-	Event EventData
-	Files []FileData
+	Event EventData  `json:"event"`
+	Files []FileData `json:"files"`
 }
 
 type EventCategoryData struct {
-	EventID           int32
-	CategoryID        int32
-	CategoryType      string
-	Price             float64
-	BookType          string
-	TotalCapacity     int32
-	AvailableCapacity int32
+	EventID           int32   `json:"event_id"`
+	CategoryID        int32   `json:"category_id"`
+	CategoryType      string  `json:"category_type"`
+	Price             float64 `json:"price"`
+	BookType          string  `json:"book_type"`
+	TotalCapacity     int32   `json:"total_capacity"`
+	AvailableCapacity int32   `json:"available_capacity"`
 }
 
 type EventImageData struct {
-	EventID      int32
-	Key          string
-	IsPrimary    bool
-	DisplayOrder int
+	EventID      int32  `json:"event_id"`
+	Key          string `json:"key"`
+	IsPrimary    bool   `json:"is_primary"`
+	DisplayOrder int    `json:"display_order"`
 }
 
 type EventDetailsData struct {
 	EventData
-	Images     []EventImageData
-	Categories []EventCategoryData
+	Images     []EventImageData    `json:"images"`
+	Categories []EventCategoryData `json:"categories"`
 }
 
 type BrowseCursor struct {
@@ -57,12 +57,12 @@ type BrowseCursor struct {
 }
 
 type BrowseFilter struct {
-	EventName string
-	Location  string
-	StartDate time.Time
-	EndDate   time.Time
-	Cursor    *BrowseCursor
-	Limit     int
+	EventName string        `json:"event_name"`
+	Location  string        `json:"location"`
+	StartDate time.Time     `json:"start_date"`
+	EndDate   time.Time     `json:"end_date"`
+	Cursor    *BrowseCursor `json:"cursor"`
+	Limit     int           `json:"limit"`
 }
 
 type BrowseResult struct {
